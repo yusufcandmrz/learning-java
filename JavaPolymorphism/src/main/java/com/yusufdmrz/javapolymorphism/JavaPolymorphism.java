ddce4a673b2708bffd0f12f0ab8eb314f9fa3b45
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2024-07-28 14:21:01 +0300

    Updated JavaPolymorphism

--- JavaPolymorphism/src/main/java/com/yusufdmrz/javapolymorphism/JavaPolymorphism.java
+++ JavaPolymorphism/src/main/java/com/yusufdmrz/javapolymorphism/JavaPolymorphism.java
@@ -17,7 +17,7 @@ class Adder {
     static int add(int a, int b, int c) {
         return a + b + c;
     }
-    
+
     static double add(double a, double b) {
         return a + b;
     }
@@ -32,7 +32,7 @@ class OverloadingCalculation {
     void sum(int a, int b, int c) {
         System.out.println(a + b + c);
     }
-    
+
     void sum(int a, int b) {
         System.out.println(a + b);
     }
@@ -45,22 +45,34 @@ class Vehicle {
     }
 }
 
-class Bike extends Vehicle {
-    
+class Bike /* extends Vehicle */ {
+
     int speed;
+    int speedlimit = 90;
 
     Bike() {
-        System.out.println("constructor is invoked");
-        System.out.println("speed -> " + speed);
-    }
-    
-    {
-        System.out.println("instance initializer block invoked");
-        speed = 100;
+        /* System.out.println("constructor is invoked");
+        System.out.println("speed -> " + speed); */
     }
 
+    /* {
+        System.out.println("instance initializer block invoked");
+        speed = 100;
+    } */
     void run() {
         System.out.println("Bike is running safely");
+    }
+}
+
+class Honda extends Bike {
+
+    int speedlimit = 150;
+}
+
+class Splendor extends Bike {
+
+    void run() {
+        System.out.println("running safely with 60km");
     }
 }
 
@@ -93,7 +105,7 @@ class AXIS extends Bank {
 }
 
 class A {
-    
+
     A() {
         System.out.println("parent class costructor invoked");
     }
@@ -108,15 +120,15 @@ class B extends A {
     B() {
         System.out.println("child class constructor invoked");
     }
-    
+
     {
         System.out.println("instance initializer block is invoked");
     }
-    
+
     B get() {
         return this;
     }
-    
+
     void message() {
         System.out.println("welcome to covariant return type");
     }
@@ -151,7 +163,7 @@ class Animal {
     Animal() {
         System.out.println("animal is created");
     }
-    
+
     void eat() {
         System.out.println("eating...");
     }
@@ -160,7 +172,7 @@ class Animal {
 class Dog extends Animal {
 
     String color = "black";
-    
+
     Dog() {
         // super();
         System.out.println("dog is created");
@@ -170,7 +182,7 @@ class Dog extends Animal {
         System.out.println(color);
         System.out.println(super.color);
     }
-    
+
     void eat() {
         System.out.println("eating bread...");
     }
@@ -182,6 +194,36 @@ class Dog extends Animal {
     void work() {
         super.eat();
         bark();
+    }
+    
+    static void method(Animal animal) {
+        /* if (animal instanceof Dog) {
+            Dog dog = (Dog) animal;
+            System.out.println("downcasting performed");
+        } */
+        Dog dog = (Dog) animal;
+        System.out.println("downcasting performed");
+    }
+}
+
+class BabyDog extends Dog {
+
+    /* void eat() {
+        System.out.println("dringking milk...");
+    } */
+}
+
+class Cat extends Animal {
+
+    void eat() {
+        System.out.println("eating rat...");
+    }
+}
+
+class Lion extends Animal {
+
+    void eat() {
+        System.out.println("eating meat...");
     }
 }
 
@@ -204,14 +246,84 @@ class Emp extends Person {
         super(id, name);
         this.salary = salary;
     }
-    
+
     void display() {
         System.out.println(id + " - " + name + " - " + salary);
     }
 }
 
+class Student {
+
+    int id;
+    String name;
+    final String PAN_CARD_NUMBER;
+
+    public Student(int id, String name, String PAN_CARD_NUMBER) {
+        this.id = id;
+        this.name = name;
+        this.PAN_CARD_NUMBER = PAN_CARD_NUMBER;
+    }
+}
+
+class Shape {
+
+    void draw() {
+        System.out.println("drawing");
+    }
+}
+
+class Rectangle extends Shape {
+
+    void draw() {
+        System.out.println("drawing rectangle");
+    }
+}
+
+class Circle extends Shape {
+
+    void draw() {
+        System.out.println("drawing circle");
+    }
+}
+
+class Triangle extends Shape {
+
+    void draw() {
+        System.out.println("drawing triangle");
+    }
+}
+
+interface Printable {}
+
+class AA implements Printable {
+
+    public void AA() {
+        System.out.println("aa method");
+    }
+}
+
+class BB implements Printable {
+
+    public void BB() {
+        System.out.println("bb method");
+    }
+}
+
+class Call {
+
+    void invoke(Printable printable) {
+        if (printable instanceof AA) {
+            AA aa = (AA) printable;
+            aa.AA();
+        } else if (printable instanceof BB) {
+            BB bb = (BB) printable;
+            bb.BB();
+        }
+    }
+}
+
 public class JavaPolymorphism {
-    
+
     /* public static void main(String arg) {
         System.out.println("main with String");
     } */
@@ -232,7 +344,7 @@ public class JavaPolymorphism {
         obj.sum(20, 20, 20);
         
         obj.sum(20, 20); */
-        
+
         
         
         // ********************************************************************;
@@ -291,5 +403,89 @@ public class JavaPolymorphism {
         
         // B
         B b = new B(); */
+        
+        
+        
+        // ********************************************************************;
+        // ********************************************************************;
+        // ********************************************************************;
+        // FINAL KEYWORD
+        // Bike
+        /* Bike bike = new Bike();
+        bike.run(); */
+        
+
+
+        // ********************************************************************;
+        // ********************************************************************;
+        // ********************************************************************;
+        // RUNTIME POLYMORPHISM
+        // Bike
+        /* Bike bike = new Splendor();
+        bike.run();
+
+        // Bank
+        Bank bank;
+        bank = new SBI();
+        System.out.println("SBI's rate of interest: " + bank.getRateOfInterest());
+        bank = new ICICI();
+        System.out.println("ICICI's rate of interest: " + bank.getRateOfInterest());
+        bank = new AXIS();
+        System.out.println("AXIS's rate of interest: " + bank.getRateOfInterest());
+
+        // Shape
+        Shape shape;
+        shape = new Rectangle();
+        shape.draw();
+        shape = new Circle();
+        shape.draw();
+        shape = new Triangle();
+        shape.draw();
+
+        // Animal
+        Animal animal;
+        animal = new Dog();
+        animal.eat();
+        animal = new Cat();
+        animal.eat();
+        animal = new Lion();
+        animal.eat();
+        
+        // Bike
+        Bike bike2 = new Honda();
+        System.out.println(bike2.speedlimit);
+        
+        // Animal
+        Animal animal2, animal3, animal4;
+        animal2 = new Animal();
+        animal3 = new Dog();
+        animal4 = new BabyDog();
+        
+        Animal animal5 = new BabyDog();
+        animal5.eat(); */
+        
+        
+        
+        // ********************************************************************;
+        // ********************************************************************;
+        // ********************************************************************;
+        // INSTANCEPF OPERATOR
+        /* JavaPolymorphism object = new JavaPolymorphism();
+        System.out.println(object instanceof JavaPolymorphism);
+        
+        // Dog
+        Dog dog = new Dog();
+        System.out.println("dog instanceof Animal -> " + (dog instanceof Animal));
+        
+        Dog dog2 = null;
+        System.out.println("dog2 instanceof Dog -> " + (dog2 instanceof Dog));
+        
+        Animal animal = new Dog();
+        Dog.method(animal);
+        
+        // Printable
+        Printable printable = new BB();
+        Call call = new Call();
+        call.invoke(printable); */
     }
 }
