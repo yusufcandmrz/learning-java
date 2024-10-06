307fe5a2efc8053a9edb5f3e333c6e13fb63491c
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2024-10-06 21:07:47 +0300

    Update JavaMultithreading

--- JavaMultithreading/src/Main.java
+++ JavaMultithreading/src/Main.java
@@ -16,8 +16,22 @@
 } */
 
 class Multi extends Thread {
+
     public void run() {
         System.out.println("thread running...");
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(500);
+                System.out.println("The current thread name is: " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i);
+        }
+    }
+
+    public Multi(String threadName) {
+        super(threadName);
     }
 }
 
@@ -78,5 +92,102 @@ public class Main /* implements Runnable */ {
         Runnable runnable = new MyThread();
         Thread thread3 = new Thread(runnable, "thread3");
         System.out.println("thread3 name: " + thread3.getName()); */
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        // SLEEPING A THREAD
+        /* Multi thread1 = new Multi();
+        Multi thread2 = new Multi();
+        thread1.start();
+        thread2.start(); */
+
+        /* for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i);
+        } */
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        // START A THREAD TWICE
+        /* Multi thread1 = new Multi();
+        thread1.start();
+        thread1.start(); */
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        // CALLING run() METHOD
+        /* Multi thread1 = new Multi();
+        Multi thread2 = new Multi();
+        thread1.run();
+        thread2.run(); */
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        // JOINING A THREAD
+        /* Multi thread1 = new Multi();
+        Multi thread2 = new Multi();
+        Multi thread3 = new Multi(); */
+
+        /* thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        thread2.start();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        thread3.start();
+        try {
+            thread3.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } */
+
+        /* thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        thread2.start();
+        thread3.start(); */
+
+        /* thread1.start();
+        try {
+            thread1.join(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        thread2.start();
+        thread3.start(); */
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        // NAMING A THREAD
+        /* Multi thread1 = new Multi();
+        System.out.println("thread1's name is: " + thread1.getName());
+        thread1.setName("Sonoo Jaiswal");
+        System.out.println("thread1's name is: " + thread1.getName()); */
+
+        /* Multi thread1 = new Multi("Javatpoint1");
+        System.out.println(thread1.getName()); */
     }
 }
