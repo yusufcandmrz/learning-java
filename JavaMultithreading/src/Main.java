7fb29927e5e7b59564352bbbde77ea280593bff2
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2024-10-12 20:26:36 +0300

    Updated JavaMultithreading

--- JavaMultithreading/src/Main.java
+++ JavaMultithreading/src/Main.java
@@ -15,10 +15,19 @@
     }
 } */
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 class Multi extends Thread {
 
     public void run() {
-        System.out.println("thread running...");
+        if (Thread.currentThread().isDaemon()) {
+            System.out.println("daemon thread running...");
+        } else {
+            System.out.println("thread running...");
+        }
         for (int i = 1; i <= 5; i++) {
             try {
                 Thread.sleep(500);
@@ -36,8 +45,70 @@ class Multi extends Thread {
 }
 
 class MyThread implements Runnable {
+
+    /* private String message;
+
+    public MyThread(String message) {
+        this.message = message;
+    }
+
     public void run() {
-        System.out.println("thread running...");
+        if (Thread.currentThread().isDaemon()) {
+            System.out.println("daemon thread running...");
+        } else {
+            System.out.println("thread running...");
+        }
+        System.out.println(Thread.currentThread().getName() + " (START) message -> " + message);
+        processMessage();
+        System.out.println(Thread.currentThread().getName() + " (END)");
+    }
+
+    private void processMessage() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    } */
+
+    private String taskName;
+
+    public MyThread(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void run() {
+
+        Date dt;
+        SimpleDateFormat sdf;
+
+        for (int i = 0; i < 5; i++) {
+            dt = new Date();
+            sdf = new SimpleDateFormat("hh:mm:ss");
+            if (i == 0) {
+                System.out.println("Initialization time for the task name: " + taskName + " = " + sdf.format(dt));
+            } else {
+                System.out.println("Time of execution for the task name: " + taskName + " = " + sdf.format(dt));
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println(taskName + " is complete");
+    }
+}
+
+class MyThreadGroup implements Runnable {
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() + " thread has finished");
     }
 }
 
@@ -189,5 +260,102 @@ public class Main /* implements Runnable */ {
 
         /* Multi thread1 = new Multi("Javatpoint1");
         System.out.println(thread1.getName()); */
+
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        // THREAD PRIORITY
+        /* Multi thread1 = new Multi("thread1");
+        System.out.println("priority of thread1: " + thread1.getPriority());
+        thread1.setPriority(7);
+        System.out.println("priority of thread1 after setting: " + thread1.getPriority());
+        System.out.println("current thread: " + Thread.currentThread().getName()
+                + ", priority: " + Thread.currentThread().getPriority()); */
+
+        /* Thread.currentThread().setPriority(7);
+        Multi thread1 = new Multi("thread1");
+        System.out.println("priority of thread1: " + thread1.getPriority()); */
+
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        //  DAEMON THREAD
+        /* Multi thread1 = new Multi("thread1");
+        Multi thread2 = new Multi("thread2");
+        thread1.setDaemon(true);
+        thread1.start();
+        thread2.start(); */
+
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        //  THREAD POOL
+        /* ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            Runnable runnable = new MyThread(" " + i);
+            executor.execute(runnable);
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+            // System.out.println("executor not terminated...");
+        }
+        System.out.println("Finished all threads"); */
+
+        /* final int MAX_TH = 3;
+        Runnable runnable1 = new MyThread("task1");
+        Runnable runnable2 = new MyThread("task2");
+        Runnable runnable3 = new MyThread("task3");
+        Runnable runnable4 = new MyThread("task4");
+        Runnable runnable5 = new MyThread("task5");
+        ExecutorService executorService = Executors.newFixedThreadPool(MAX_TH);
+        executorService.execute(runnable1);
+        executorService.execute(runnable2);
+        executorService.execute(runnable3);
+        executorService.execute(runnable4);
+        executorService.execute(runnable5);
+        executorService.shutdown(); */
+
+
+
+        // *******************************************
+        // *******************************************
+        // *******************************************
+        //  THREAD GROUP
+        /* MyThreadGroup myThreadGroup = new MyThreadGroup();
+        ThreadGroup threadGroup = new ThreadGroup("ThreadGroupName");
+        ThreadGroup threadGroup2 = new ThreadGroup(threadGroup,"ThreadGroupName2");
+
+        Thread thread1 = new Thread(threadGroup, myThreadGroup, "one");
+        thread1.start();
+        Thread thread2 = new Thread(threadGroup, myThreadGroup, "two");
+        thread2.start();
+        Thread thread3 = new Thread(threadGroup, myThreadGroup, "three");
+        thread3.start(); */
+
+        /* System.out.println("Thread Group Name: " + threadGroup.getName());
+        threadGroup.list(); */
+
+        // System.out.println("Active Threads: " + threadGroup.activeCount());
+
+        // System.out.println("Active Thread Grops: " + threadGroup.activeGroupCount());
+
+        /* Thread[] threads = new Thread[threadGroup.activeCount()];
+        int count = threadGroup.enumerate(threads);
+        for(int i = 0; i < count; i++) {
+            System.out.println("Thread " + threads[i].getName() + " is found");
+        } */
+
+        // System.out.println("Thread Group Max Priority: " + threadGroup.getMaxPriority());
+
+        /* System.out.println("ThreadGroup Parent Name: " + threadGroup.getParent().getName());
+        System.out.println("ThreadGroup2 Parent Name: " + threadGroup2.getParent().getName()); */
+
+        // threadGroup.interrupt();
     }
 }
