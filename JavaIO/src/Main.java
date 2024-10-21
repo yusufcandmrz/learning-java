0233799e5f8244e1b606eee54997f9d839231414
Yusuf Demiroz y00858166 <93606208+yusufcandmrz@users.noreply.github.com>
2024-10-21 16:52:24 +0300

    Added JavaIO

---
+++ JavaIO/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // JAVA INPUT/OUTPUT
+        /* System.out.println("simple message");
+        System.err.println("error message");
+
+        try {
+            int i = System.in.read();
+            System.out.println((char) i);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } */
+
+
+        // ****************************************************
+        // ****************************************************
+        // ****************************************************
+        // FileOutputStream
+        /* try {
+            FileOutputStream fout = new FileOutputStream("D:\\java\\learning-java\\JavaIO\\testFile.txt");
+            String string = "JavaTpoint";
+            byte[] byes = string.getBytes();
+            fout.write(byes);
+            fout.close();
+            System.out.println("success...");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } */
+    }
+}
