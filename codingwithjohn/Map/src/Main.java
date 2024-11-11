ab8fa1707daa7084e4e92d46d7042307f11011db
Yusuf Demiroz 00858166 <yusufcandmrz@gmail.com>
2024-11-11 16:59:13 +0300

    Added Map in codingwithjohn

---
+++ codingwithjohn/Map/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* HashMap<Integer, String> users = new HashMap<>();
+        users.put(1, "Yusuf");
+        users.put(2, "Taha");
+        users.put(2, "Taha Huseyin");
+        System.out.println("users contains key 1? " + users.containsKey(1));
+        System.out.println("users contains value Muhammet? " + users.containsValue("Muhammet"));
+        users.putIfAbsent(2, "Taha");
+        users.remove(2);
+        users.put(3, "Sait");
+        users.replace(3, "Taha");
+        System.out.println(users); */
+    }
+}
