9cbb419b851452f264132e0120c55352839ff818
Yusuf Demiroz 84396111 <yusufcandmrz@gmail.com>
2025-03-07 10:14:23 +0300

    builder added

---
+++ creational-design-patterns/builder/src/main/java/org/example/Computer.java
@@ -0,0 +1,61 @@
+package org.example;
+
+public class Computer {
+
+    private String HDD;
+    private String RAM;
+
+    private boolean isGraphicsCardEnabled;
+    private boolean isBluetoothEnabled;
+
+    private Computer(ComputerBuilder builder) {
+        this.HDD = builder.HDD;
+        this.RAM = builder.HDD;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+        this.isBluetoothEnabled = builder.isBluetoothEnabled;
+    }
+
+    public String getHDD() {
+        return HDD;
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+
+    public boolean isGraphicsCardEnabled() {
+        return isGraphicsCardEnabled;
+    }
+
+    public boolean isBluetoothEnabled() {
+        return isBluetoothEnabled;
+    }
+
+    public static class ComputerBuilder {
+
+        private String HDD;
+        private String RAM;
+
+        private boolean isGraphicsCardEnabled;
+        private boolean isBluetoothEnabled;
+
+        public ComputerBuilder(String HDD, String RAM) {
+            this.HDD = HDD;
+            this.RAM = RAM;
+        }
+
+        public ComputerBuilder setIsGraphicsCardEnabled(boolean isGraphicsCardEnabled) {
+            this.isGraphicsCardEnabled = isGraphicsCardEnabled;
+            return this;
+        }
+
+        public ComputerBuilder setIsBluetoothEnabled(boolean isBluetoothEnabled) {
+            this.isBluetoothEnabled = isBluetoothEnabled;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
