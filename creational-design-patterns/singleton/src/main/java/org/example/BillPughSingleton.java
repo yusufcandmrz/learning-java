4de3bc184fc459cbde160f8c240032f0ad9e34d6
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-03-17 22:56:32 +0300

    BillPughSingleton design pattern added

---
+++ creational-design-patterns/singleton/src/main/java/org/example/BillPughSingleton.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class BillPughSingleton {
+
+    private BillPughSingleton() {
+    }
+
+    private static class SingletonHelper {
+        private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+    }
+
+    public static BillPughSingleton getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+}
