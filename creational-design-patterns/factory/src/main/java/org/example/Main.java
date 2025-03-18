696e3178e3e88e555ccdc32c8af584a2c35bc63e
Yusuf Demiroz 84396111 <yusufcandmrz@gmail.com>
2025-03-18 13:23:09 +0300

    added test for factory design

--- creational-design-patterns/factory/src/main/java/org/example/Main.java
+++ creational-design-patterns/factory/src/main/java/org/example/Main.java
@@ -2,5 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Computer computerOne = ComputerFactory.getComputer("PC", "RAM one", "HDD one", "CPU one");
+        Computer computerTwo = ComputerFactory.getComputer("SERVER", "RAM two", "HDD two", "CPU two");
+        System.out.println("PC -> " + computerOne);
+        System.out.println("SERVER -> " + computerTwo);
     }
 }
